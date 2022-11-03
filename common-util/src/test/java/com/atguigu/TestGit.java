215d4502ef3361b501474f4e94b8021d4596ca50
Shuain <1505585544@qq.com>
2022-11-03 19:31:11 +0800

    修改了TestGit文件添加了TestCommit方法

--- common-util/src/test/java/com/atguigu/TestGit.java
+++ common-util/src/test/java/com/atguigu/TestGit.java
@@ -6,5 +6,11 @@ public class TestGit {
     @Test
     public  void  testAdd(){
         System.out.println("git.add");
+
+    }
+    @Test
+    public  void  testCommit(){
+        System.out.println("git.Commit");
+
     }
 }
