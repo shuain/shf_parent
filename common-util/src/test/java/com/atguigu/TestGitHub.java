5b36317be32ab2519e4fc52dbd6f11f6961d5c67
shuain <57972206+shuain@users.noreply.github.com>
2022-11-04 08:30:39 +0800

    Update TestGitHub.java

--- common-util/src/test/java/com/atguigu/TestGitHub.java
+++ common-util/src/test/java/com/atguigu/TestGitHub.java
@@ -7,4 +7,8 @@ public class TestGitHub {
     public void testGitHub(){
         System.out.println("上传至GitHub");
     }
+    @Test
+    public void testGitHub2(){
+        System.out.println("从GitHub拉取");
+    }
 }
