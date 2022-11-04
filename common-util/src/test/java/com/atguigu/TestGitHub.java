2ea698a03d16db372cd1ff58cab872cf0e113fd1
Shuain <1505585544@qq.com>
2022-11-04 08:23:11 +0800

    添加了TestGit

---
+++ common-util/src/test/java/com/atguigu/TestGitHub.java
@@ -0,0 +1,10 @@
+package com.atguigu;
+
+import org.junit.jupiter.api.Test;
+
+public class TestGitHub {
+    @Test
+    public void testGitHub(){
+        System.out.println("上传至GitHub");
+    }
+}
